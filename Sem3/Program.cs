752c2755d6fb75e5e89e3dfc403dfbc1d34ff08e
Burshtyn Tanya <kruamge@gmail.com>
2023-06-30 12:58:08 +0300

    df

--- Sem3/Program.cs
+++ Sem3/Program.cs
@@ -28,31 +28,31 @@ else Console.WriteLine($"Введи пятизначное число");
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-// Console.Write("Введите координату по оси OX для второй точки: ");
-// int x2 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите координату по оси OY для второй точки: ");
-// int y2 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите координату по оси OZ для второй точки: ");
-// int z2 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите координату по оси OX для первой точки: ");
-// int x1 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите координату по оси OY для первой точки: ");
-// int y1 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите координату по оси OZ для первой точки: ");
-// int z1 = Convert.ToInt32(Console.ReadLine());
-// double  d = (Math.Round(Math.Pow(x2-x1, 2) + Math.Pow((y2-y1), 2)+ Math.Pow(z2-z1, 2), 2));
-// Console.WriteLine($"расстояние: {d}");
+Console.Write("Введите координату по оси OX для второй точки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату по оси OY для второй точки: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату по оси OZ для второй точки: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату по оси OX для первой точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату по оси OY для первой точки: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату по оси OZ для первой точки: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+double  d = (Math.Round(Math.Pow(x2-x1, 2) + Math.Pow((y2-y1), 2)+ Math.Pow(z2-z1, 2), 2));
+Console.WriteLine($"расстояние: {d}");
 
 // Напишите программу, которая принимает 
 // на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
  
-//  int start =1;
-// Console.Write("Введите число: ");
-// int end = Convert.ToInt32(Console.ReadLine());
-// while (start<= end)
-// {
-//     Console.Write(Math.Pow(start, 3) + ",");
-//     start++;
-// }
+ int start =1;
+Console.Write("Введите число: ");
+int end = Convert.ToInt32(Console.ReadLine());
+while (start<= end)
+{
+    Console.Write(Math.Pow(start, 3) + ",");
+    start++;
+}
